46ceb9096a362b502d08043ecee9232b2860e44c
DehydrateBrickworker <no_1379@outlook.com>
2023-10-31 10:49:20 +0800

    20231031 添加查询两个数据框特定字段内元素内容不同的字段 20231023 添加Excel表格及其内容是否存在检测的功能 20231016 mkDirOrFile功能更改创建xlsx与xlsm文件方式;添加路径格式不正确报错 20231010 添加参数接收变量类型的校验功能

--- rpaAbout/rpaPyScriptFunc.py
+++ rpaAbout/rpaPyScriptFunc.py
@@ -13,9 +13,12 @@ def fileAbsentCheck(path):
     contentIsNone = True
     tableIsExist = os.path.exists(path)
     if tableIsExist:
-        df_Table = pd.read_excel(path)
-        if df_Table.shape[0] > 0:
-            tableIsExist = True
+        if os.path.getsize(path) > 0:
+            df_Table = pd.read_excel(path)
+            if df_Table.shape[0] > 0:
+                tableIsExist = True
+            else:
+                tableIsExist = False
         else:
             tableIsExist = False
 
@@ -255,6 +258,120 @@ def writeToExcel(path:str, values:dict, sheetname=0, columns:list=None, istransp
     wb.close()
     app.quit()
 
+def dfDiffContentHandle(df:list, connect_condition, find_what:dict, sec_table_sync=False):
+    '''
+    两个通过具有唯一性的查询条件，比较各字典是否不同
+    :param df: list [主数据框, 副数据框]
+    :param connect_condition: list or str [查询条件字段] or [主数据框查询条件字段, 副数据框查询条件字段]
+    :param find_what: dict 需要查询的内容是否不同的字段的字典 {主数据框字段 : 对应主数据框字段的副数据框字段}
+    :param sec_table_sync: bool 是否需要按照副数据框字段的内容更改主数据框字段的内容
+    :return:
+    '''
+    import pandas
+
+    class parmTypeError(Exception):
+        def __init__(self, varname):
+            self.varname = varname
+
+    dict_parmTypeJudge = {
+        'df' : (type(df) is list) & (False not in [type(x) is pandas.core.frame.DataFrame for x in df]),
+        'connect_condition' : (type(connect_condition) is str) | \
+            ((type(connect_condition) is list) & (False not in [type(x) is str for x in connect_condition if type(connect_condition) is list]) & (len(connect_condition) == 2 if type(connect_condition) is list else False)),
+        'find_what' : ((type(find_what) is dict) & (False not in [type(x) is str for x in find_what.keys() if type(find_what) is dict]) & (False not in [type(x) is str for x in find_what.values() if type(find_what) is dict])) | \
+                      ((type(find_what) is list) & (False not in [type(x) is str for x in find_what if type(find_what) is list])),
+        'sec_table_sync' : type(sec_table_sync) is bool,
+    }
+
+    for parm in dict_parmTypeJudge.keys():
+        if dict_parmTypeJudge[parm] == False:
+            raise parmTypeError(parm)
+
+    # 判断参数connect_condition是否为一个列表，如果为列表，则将列表的0号元素与1号元素分别赋值给mainConnectField、secConnectField
+    if connect_condition is list:
+        # 主数据框查找条件字段名
+        mainConnectField = connect_condition[0]
+        # 副数据框查找条件字段名
+        secConnectField = connect_condition[1]
+    else:
+        mainConnectField = connect_condition
+        secConnectField = connect_condition
+
+    # 通过查找条件找出内容不同的字段
+    def finddfDiffContent():
+
+        # 初始化存储内容不同字段的字典
+        dict_DiffContent = {}
+
+        # 遍历主数据框中查询条件的元素
+        for condition_content in df[0][mainConnectField].values.tolist():
+            # 初始化存储内容不同字段(key)及其对应主副数据框元素(value)的字典
+            dict_DiffContentColumns = {}
+
+            # 判断当前查询条件元素在主副数据框中是否唯一
+            if (df[0][mainConnectField].values.tolist().count(condition_content) > 1) | (df[1][secConnectField].values.tolist().count(condition_content) > 1):
+                # 如果不唯一，则在返回结果的字典中标记并不再查找此查询条件元素的字段内容
+                dict_DiffContent[mainConnectField] = 'main or secondary table non unique'
+                continue
+
+            # 遍历需要查询的内容不同字段的字段名称
+            for main_column in find_what.keys():
+
+                # 确定主数据框当前字段对应的值
+                main_DfContent = df[0][df[0][mainConnectField] == condition_content][main_column]
+                # 判断字段对应的值是否为空
+                if len(main_DfContent.values) == 1:
+                    main_DfContent = main_DfContent.values[0]
+                elif len(main_DfContent) == 0:
+                    main_DfContent = None
+                # 确定副数据框当前字段对应的值
+                sec_DfContent = df[1][df[1][secConnectField] == condition_content][find_what[main_column]].values
+                # 判断字段对应的值是否为空
+                if len(sec_DfContent) == 1:
+                    sec_DfContent = sec_DfContent[0]
+                elif len(sec_DfContent) == 0:
+                    sec_DfContent = None
+
+                # 判断当前字段主副数据框的值是否相等
+                if main_DfContent != sec_DfContent:
+                    # 将不相等的字段及其值存储到dict_DiffContentColumns字典中
+                    dict_DiffContentColumns[main_column] = [main_DfContent, sec_DfContent]
+
+            # 判断当前查询条件元素是否存在内容不同的字段
+            if (dict_DiffContentColumns.__len__() > 0 if type(dict_DiffContentColumns) is dict else type(dict_DiffContentColumns) is str):
+                # 将内容不同的查询条件元素及其比较情况存储到dict_DiffContent字典中
+                dict_DiffContent[condition_content] = dict_DiffContentColumns
+
+        return dict_DiffContent
+
+    # 更改主数据框中与副数据框比较字段内容不同值为副数据框的值
+    def changeDiffContent():
+        # 遍历存在内容不同字段的查找条件元素
+        for condition_content in finddfDiffContent().keys():
+
+            # 确定其在主数据框的行索引
+            mainDfIndex = df[0][df[0][mainConnectField] == condition_content].index
+            if len(mainDfIndex) > 0:
+                mainDfIndex = mainDfIndex[0]
+
+            # 判断当前元素是否不存在内容不同的字段，或者其是否为字符串
+            if (finddfDiffContent()[condition_content].__len__() == 0) | (type(finddfDiffContent()[condition_content]) is str):
+                continue
+
+            # 遍历内容不同的字段名
+            for column_name in finddfDiffContent()[condition_content].keys():
+
+                # 更改主数据框中的元素
+                df[0].loc[mainDfIndex, column_name] = finddfDiffContent()[condition_content][column_name][1]
+
+        return df[0]
+
+    if sec_table_sync is True:
+
+        return finddfDiffContent(), changeDiffContent()
+
+    elif sec_table_sync is False:
+
+        return finddfDiffContent()
 
 # 日期相关计算
 import datetime
